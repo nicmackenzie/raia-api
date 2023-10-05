ce1e3a798d45fd7001a5269cfdde227b702305ea
Isaac-ngahu <isaac.ngahu@student.moringaschool.com>
2023-10-05 21:22:30 +0300

    creating the routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :reviews
+  resources :messages
+  resources :discussion_replies
+  resources :discussions
 end
