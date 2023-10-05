e9b7e4e2ecaf64b3d6d9706812fd6e45d48290ea
Isaac-ngahu <isaac.ngahu@student.moringaschool.com>
2023-10-05 21:50:12 +0300

    adding the relation between discussion and discussion replies

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -1,3 +1,4 @@
 class Discussion < ApplicationRecord
+  has_many discussion_replies
   belongs_to :user
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,4 +9,7 @@ puts "Seeding database..."
 # user1 = User.create(name:"John",role:"Doctor")
 # user2 = User.create(name:"Jack",role:"Chief")
 # user3 = User.create(name:"Eustus",role:"Developer")
+discussion1 = Discussion.create(title:"Climate change",content:"The weather patterns recently have drastically changed",user_id:1,is_deleted:false)
+discussion2 = Discussion.create(title:"Roads status",content:"The roads built about 10 years back are in very poor condition at the moment",user_id:2,is_deleted:false)
+discussion3 = Discussion.create(title:"Networking opportunities",content:"there are very few networking seminars for the youth",user_id:3,is_deleted:false)
 puts "Done seeding"
