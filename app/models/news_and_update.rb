dc0afabe9517b1aa5e086d4ce6ec289d01c6017a
Ivar-7 <leank6071@gmail.com>
2023-10-06 19:10:13 +0300

    Added relationship

--- app/models/news_and_update.rb
+++ app/models/news_and_update.rb
@@ -1,4 +1,4 @@
-class NewsAndUpdate < ApplicationRecord
+class NewsAndUpdates < ApplicationRecord
   belongs_to :county
-  belongs_to :user
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
 end
