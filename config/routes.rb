e22ed430b78fde0c1e3ef0f81562a2129bf28910
maimuna omar <maymunaabdi07@gmail.com>
2023-10-06 19:23:33 +0300

    created session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
