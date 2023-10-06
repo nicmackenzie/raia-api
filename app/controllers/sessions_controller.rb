794891c1d124778e0f3f1c378e72c2f453b1d128
maimuna omar <maymunaabdi07@gmail.com>
2023-10-06 20:31:27 +0300

    updated session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,16 @@
 class SessionsController < ApplicationController
-  def new
-  end
-
   def create
+    user = User.find_by(email: params[:email])
+    if user&.authenticate(params[:password])
+      session[:user_id] = user.id
+      render json: { message: 'Logged in successfully', user: user }, status: :ok
+    else
+      render json: { error: 'Invalid email or password' }, status: :unauthorized
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    render json: { message: 'Logged out successfully' }, status: :ok
   end
 end
