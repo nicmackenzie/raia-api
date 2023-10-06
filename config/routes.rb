57394b0f2b49244fa29b84e8eca3087ed53fcf65
Ivar-7 <leank6071@gmail.com>
2023-10-06 19:17:13 +0300

    routes for petitions, news, event, tickets

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # Routes for Petitions
+  resources :petitions
+  # Routes for NewsAndUpdates
+  resources :news_and_updates
+  # Routes for Events
+  resources :events
+  # Routes for Tickets
+  resources :tickets
 end
