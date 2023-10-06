79405352c3979a200ceb9303347289080cdd217c
maimuna omar <maymunaabdi07@gmail.com>
2023-10-06 19:22:26 +0300

    worked on user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ApplicationRecord
-end
+    # Relationships
+    belongs_to :county, optional: true
+    has_many :interests
+    # add other relationships here as needed...
+  
+    has_secure_password
+ 
+    # Enums
+    enum role: { citizen: 0, leader: 1, admin: 2 }
+  
+    # Validations
+    validates :email, presence: true, uniqueness: true
+    validates :full_name, presence: true
+    # add other validations here as needed...
+  end
+  
