b533034b36d866eebab9565823baefc9cb9db0cf
Isaac-ngahu <isaac.ngahu@student.moringaschool.com>
2023-10-08 13:09:53 +0300

    reverting migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_07_202003) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_05_180015) do
   create_table "discussion_replies", force: :cascade do |t|
     t.integer "discussion_id", null: false
     t.integer "user_id", null: false
@@ -43,11 +43,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_07_202003) do
   create_table "reviews", force: :cascade do |t|
     t.text "content"
     t.decimal "rating"
-    t.integer "reviewer_id", null: false
+    t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "leader_id"
-    t.index ["reviewer_id"], name: "index_reviews_on_reviewer_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -60,5 +59,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_07_202003) do
   add_foreign_key "discussion_replies", "discussions"
   add_foreign_key "discussion_replies", "users"
   add_foreign_key "discussions", "users"
-  add_foreign_key "reviews", "users", column: "reviewer_id"
+  add_foreign_key "reviews", "users"
 end
