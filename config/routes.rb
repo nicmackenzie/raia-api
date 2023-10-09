c64e7bb4dd882375a963b11d83b04dc767a4cc4a
nicmackenzie <nicmackenzie@live.com>
2023-10-09 08:41:06 +0300

    add route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users, only: [:index, :show,:update,:destroy]
   resource :session, only: [:create, :destroy]
 
   # ... any other routes ...
+  post 'sign-up', to: 'users#create'
 end
