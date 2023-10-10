e8449f1eb34f8812e0fb7fb1e2fb275c97582c26
maimuna omar <maymunaabdi07@gmail.com>
2023-10-10 14:38:50 +0300

    worked on routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  # Custom route for user registration
+  post '/signup', to: 'users#create'
+
+  # Standard RESTful routes for users
   resources :users
+
+  # Session routes for login and logout
   resource :session, only: [:create, :destroy]
 
   # ... any other routes ...
