1eb7bea9212cea2e7db83d669acec51337d84c49
maimuna omar <maymunaabdi07@gmail.com>
2023-10-10 14:52:05 +0300

    worked on county model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_10_114532) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_10_115059) do
+  create_table "counties", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "interests", force: :cascade do |t|
     t.string "name"
     t.integer "user_id", null: false
