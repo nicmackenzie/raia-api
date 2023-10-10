13c598631c9209f9d55cd6b38af8abde00466287
maimuna omar <maymunaabdi07@gmail.com>
2023-10-10 14:25:54 +0300

    updated application controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,14 +15,12 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    # session[:user_id] = nil
     render json: { message: 'Logged out successfully' }, status: :ok
   end
 
   private
 
   def generate_jwt_token(user)
-    # Implement your JWT token generation logic here
     payload = { sub: user.id, exp: 24.hours.from_now.to_i }
     JWT.encode(payload, ENV['JWT_SECRET'], 'HS256')
   end
