dd97820cf8d819ac67257b93685fd4c1587e305f
Isaac-ngahu <isaac.ngahu@student.moringaschool.com>
2023-10-11 20:45:16 +0300

    adding sender and receiver validations to messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,13 @@
 class MessagesController < ApplicationController
+    def index
+
+    end
+    def create
+        message = Message.create!(message_params)
+    end
+
+    private
+    def message_params
+        params.permit(:sender_id,:receiver_id,:content)
+    end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,13 @@
 class Message < ApplicationRecord
     belongs_to :sender,class_name: 'User',foreign_key: 'sender_id'
     belongs_to :receiver,class_name: 'User',foreign_key: 'receiver_id'
+    validates :sender_id,presence: true
+    validates :receiver_id,presence: true
+    validate :sender_and_receiver_not_the_same
+
+    def sender_and_receiver_not_the_same
+        if sender_id == receiver_id
+            error.add(:base,"Sender and receiver cannot be the same")
+        end
+    end
 end
