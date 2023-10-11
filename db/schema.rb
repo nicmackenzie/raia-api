4e00ca1ec6d7598594bbe8244438f44c3b416fca
nicmackenzie <nicmackenzie@live.com>
2023-10-11 11:51:54 +0300

    add default value

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_10_115059) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_11_085027) do
   create_table "counties", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -87,8 +87,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_10_115059) do
     t.string "profile_image"
     t.string "password_digest"
     t.boolean "verified"
-    t.boolean "active"
-    t.boolean "is_deleted"
+    t.boolean "active", default: true
+    t.boolean "is_deleted", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "user_uid"
