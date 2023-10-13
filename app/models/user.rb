9f03c2b2d984bf04c4f152a84a39ba61c9449cdd
Isaac-ngahu <isaac.ngahu@student.moringaschool.com>
2023-10-13 09:02:38 +0300

    solving merge conflicts

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,10 +25,4 @@ class User < ApplicationRecord
   
     # ... rest of the model code ...
   end
-  class User < ApplicationRecord
-    has_many :discussion_replies
-    has_many :discussions
-    has_many :reviews
-    has_many :messages_sent,class_name: 'Message', foreign_key: 'sender_id'
-    has_many :messages_received,class_name: 'Message', foreign_key: 'receiver_id'
-end
+ 
