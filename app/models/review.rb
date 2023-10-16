768e1df111cd115ecc4defebe14294a93c990a1a
Isaac-ngahu <isaac.ngahu@student.moringaschool.com>
2023-10-16 13:16:03 +0300

    adding changes to review model in development

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :leader,class_name: 'User',foreign_key: 'leader_id'
   belongs_to :reviewer,class_name: 'User',foreign_key: 'reviewer_id'
-
+ validates :content,:category,:reviewer_id,:leader_id,:rating,presence: true
 end
