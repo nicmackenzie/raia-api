f9f1334f8926b53e2a95daa4c168a9bbf46115df
Ivar-7 <leank6071@gmail.com>
2023-10-16 20:35:55 +0300

    rename

---
+++ app/models/news_and_update_comment.rb
@@ -0,0 +1,3 @@
+class NewsAndUpdateComment < ApplicationRecord
+  belongs_to :news_and_updates
+end
--- app/models/news_and_updates_comment.rb
+++
@@ -1,3 +0,0 @@
-class NewsAndUpdatesComment < ApplicationRecord
-  belongs_to :news_and_updates
-end
