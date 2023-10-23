7af96652c9a2d91e90f26548483b90edb88fbd7f
Ivar-7 <leank6071@gmail.com>
2023-10-23 15:02:35 +0300

    can post message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,37 +1,3 @@
-# class MessagesController < ApplicationController
-# rescue_from ActiveRecord::RecordInvalid, with: :render_record_invalid
-# rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
-#     wrap_parameters format: []
-#     def show
-#         message = find_message(params[:id])
-#         render json: message, status: :ok
-#     end
-#     def create
-#         message = Message.create!(message_params)
-#         render json: message, status: :created
-#     end
-#     def destroy
-#         message = find_message(params[:id])
-#         message.destroy
-#         head :no_content
-#     end
-
-#     private
-#     def message_params
-#         params.permit(:sender_id,:receiver_id,:content)
-#     end
-#     def render_record_invalid(invalid)
-#         render json: {errors:[invalid.record.errors.full_messages]},status: :unprocessable_entity
-#     end
-#     def render_not_found
-#         render json: {error:"Message not found"},status: :not_found
-#     end
-#     def find_message(id)
-#         message = Message.find(id)
-#     end
-# end
-
-
 class MessagesController < ApplicationController
     # before_action :set_message, only: [:show, :destroy]
     
@@ -54,7 +20,7 @@ class MessagesController < ApplicationController
     def create
         # sender should be the current user
         # sender = User.find_by(username: message_params[:sender])
-        receiver = User.find_by(username: message_params[:receiver])
+        receiver = User.find_by(email: message_params[:receiver])
         
         if receiver
           @message = Message.new(sender_id: message_params[:sender_id], receiver_id: receiver.id, content: message_params[:content])
@@ -80,7 +46,7 @@ class MessagesController < ApplicationController
     
     # Use strong parameters to allow specific data through
     def message_params
-        params.require(:message).permit(:sender_id, :receiver, :content)
+        params.permit(:sender_id, :receiver, :content)
     end
 
     # Handle RecordInvalid errors
