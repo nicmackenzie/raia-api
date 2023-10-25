fecfa1d0a3d87c48faece266633b10336cbae0ac
Isaac-ngahu <isaac.ngahu@student.moringaschool.com>
2023-10-25 12:01:04 +0300

    adding column to discussions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_20_094453) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_23_195741) do
   create_table "counties", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -35,6 +35,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_20_094453) do
     t.boolean "is_deleted"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "discussion_resources"
     t.index ["user_id"], name: "index_discussions_on_user_id"
   end
 
